5e59775991085b2f18e9f36f0b7f44105b1d16ef
european <artik-54@mail.ru>
2021-04-29 13:38:04 +0700

    new

--- task_1.py
+++ task_1.py
@@ -13,8 +13,32 @@ list = sample(range(-10, 11), 20)
 
 print(list)
 
+import timeit
+
+code_to_test = """
+a = range(100000)
+b = []
+for i in a:
+    b.append(i*2)
+"""
+
+elapsed_time = timeit.timeit(code_to_test, number=100)/100
+print(elapsed_time)
+
 for a in range(20):
     k=str(a+1)+' '+'may'
     d={k:a}
     print(d)
 print('Весь словарь имеет вид: ',d)
+
+import timeit
+
+code_to_test = """
+a = range(100000)
+b = []
+for i in a:
+    b.append(i*2)
+"""
+
+elapsed_time = timeit.timeit(code_to_test, number=100)/100
+print(elapsed_time)
