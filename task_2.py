8ade61d62b1006f1fc685f340e2329b148f46fe6
european <artik-54@mail.ru>
2021-04-29 14:22:57 +0700

    new

--- task_2.py
+++ task_2.py
@@ -13,3 +13,60 @@
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
 """
+import hashlib
+import os
+
+users = {}  # Простое демо хранилище
+
+# Add a user
+username = 'Brent'  # Имя пользователя
+password = 'mypassword'  # Пароль пользователя
+
+salt = os.urandom(32)  # Новая соль для данного пользователя
+key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+users[username] = {  # Хранение ключа и соли
+    'salt': salt,
+    'key': key
+}
+
+# Попытка проверки 1 (неправильный пароль)
+username = 'Brent'
+password = 'notmypassword'
+
+salt = users[username]['salt']  # Получение соли
+key = users[username]['key']  # Получение правильного ключа
+new_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+assert key != new_key  # Ключи не совпадают, следовательно, пароли не совпадают
+
+# Попытка проверки 2 (правильный пароль)
+username = 'Brent'
+password = 'mypassword'
+
+salt = users[username]['salt']
+key = users[username]['key']
+new_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+assert key == new_key  # Ключи совпадают, следовательно, и пароли совпадают
+
+# Добавление нового пользователя
+username = 'Jarrod'
+password = 'my$ecur3p@$$w0rd'
+
+salt = os.urandom(32)  # Новая соль для данного пользователя
+key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+users[username] = {
+    'salt': salt,
+    'key': key
+}
+
+# Проверяем правильно ли введен пароль
+username = 'Jarrod'
+password = 'my$ecur3p@$$w0rd'
+
+salt = users[username]['salt']
+key = users[username]['key']
+new_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+assert key == new_key  # Ключи совпадают, поэтому совпадают пароли и у этого пользователя
+
